c8bd956a4857d3d0b50875c06edd48256761b31e
eehd80 <eehd80yul@gmail.com>
2023-10-10 18:21:15 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from './components/layout/Layout';
+
+function App() {
+    return <Layout>이 글씨는 app.js에서 쓴 글</Layout>;
+}
+
+export default App;
---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// function Header() {}
+const Header = () => {
+    return (
+        <header>
+            <h1>dashboard</h1>
+        </header>
+    );
+};
+
+export default Header;

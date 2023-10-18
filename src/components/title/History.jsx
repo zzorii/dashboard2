740bd7684f790db5a21fad9afd7019589e9d9c0f
eehd80 <eehd80yul@gmail.com>
2023-10-18 12:31:05 +0900

    history props

--- src/components/title/History.jsx
+++ src/components/title/History.jsx
@@ -1,7 +1,8 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from '@chakra-ui/react';
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-const History = () => {
+const History = ({ pageName }) => {
     return (
         <Breadcrumb color="secondary_grey_700" fontSize="14px" fontWeight="500">
             <BreadcrumbItem>
@@ -10,8 +11,8 @@ const History = () => {
                 </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbItem isCurrentPage>
-                <BreadcrumbLink as={Link} to="/tables">
-                    tables
+                <BreadcrumbLink as={Link} to={`/${pageName.toLowerCase()}`}>
+                    {pageName}
                 </BreadcrumbLink>
             </BreadcrumbItem>
         </Breadcrumb>
--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -7,7 +7,7 @@ const Tables = () => {
     return (
         <>
             <Box pl="10px">
-                <History />
+                <History pageName={'table'} />
                 <Title title="Tables" desc="이 페이지는 tables 페이지 입니다." />
             </Box>
         </>

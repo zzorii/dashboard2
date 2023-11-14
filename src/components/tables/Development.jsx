9e81fca82a50bbc5fb8917bae907e3fbfaac2092
nadahelly <nadahelly@naver.com>
2023-11-14 12:29:53 +0900

    h

--- src/components/tables/Development.jsx
+++ src/components/tables/Development.jsx
@@ -18,9 +18,9 @@ import {
   Flex,
   Box,
 } from "@chakra-ui/react";
-import apple from "../../assets/images/tables/apple.svg";
-import android from "../../assets/images/tables/android.svg";
-import windows from "../../assets/images/tables/windows.svg";
+import apple from "../../assets/img/tables/apple.svg";
+import android from "../../assets/img/tables/android.svg";
+import windows from "../../assets/img/tables/windows.svg";
 
 const data = [
   {
